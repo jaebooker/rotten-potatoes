9810ac658d6d3acb2203c46fe55ea94de6f4a05a
jaebooker <jaesonbooker@gmail.com>
2018-10-22 18:52:54 -0700

    first

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.get('/', (req, res) => {
+  res.send('Mornin, Starshine!')
+})
+
+app.listen(3000, () => {
+  console.log('App listening on port 3000!')
+})
