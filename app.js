046eb82915a4e3ef23fca3b15746fbf20cd15c72
jaebooker <jaesonbooker@gmail.com>
2018-10-22 21:34:49 -0700

    added create

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect('mongodb://localhost/rotten-potatoes');
 const Review = mongoose.model('Review', {
   title: String,
-  movieTitle: String
+  movieTitle: String,
+  description: String
 });
 var exphbs = require('express-handlebars');
 let reviews = [
@@ -28,3 +31,16 @@ app.get('/', (req, res) => {
     })
   //res.render('reviews-index', { reviews: reviews });
 })
+app.get('/reviews/new', (req, res) => {
+    res.render('reviews-new', {});
+})
+app.post('/reviews', (req, res) => {
+    Review.create(req.body).then((review) => {
+        console.log("creating a new review in 5...")
+        res.redirect('/');
+    }).catch((err) => {
+        console.log(err.message);
+    })
+})
+
+//Terrontino has released perhaps his finest work in this long-anticipated Gone With The Wind sequel, starring Jennifer Lawrence as a very captivating Scarlet, and Benedict Cumberbatch as the devious, but sexy, Rhett Butler. The film begins ten minutes after the first one ended, when Yankee deserters from the war decide to try to take Terra by force, but Scarlet is having none of it!
