7cbd48b7cfa1db370713c95240044fa1ef7efe45
jaebooker <jaesonbooker@gmail.com>
2018-10-22 23:28:16 -0700

    added edit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect('mongodb://localhost/rotten-potatoes');
 const Review = mongoose.model('Review', {
@@ -21,6 +22,7 @@ app.listen(3000, () => {
   console.log('App listening on port 3000!')
 })
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.use(methodOverride('_method'))
 app.set('view engine', 'handlebars');
 app.get('/', (req, res) => {
     Review.find()
@@ -50,5 +52,18 @@ app.get('/reviews/:id', (req, res) => {
         console.log(err.message);
     })
 });
-
+app.get('/reviews/:id/edit', (req, res) => {
+    Review.findById(req.params.id, function(err, review) {
+        res.render('reviews-edit', { review: review });
+    })
+})
+app.put('/reviews/:id/', (req, res) => {
+    Review.findByIdAndUpdate(req.params.id, req.body)
+    .then(review => {
+        res.redirect(`/reviews/${review._id}`)
+    })
+    .catch(err => {
+        console.log(err.message)
+    })
+})
 //Terrontino has released perhaps his finest work in this long-anticipated Gone With The Wind sequel, starring Jennifer Lawrence as a very captivating Scarlet, and Benedict Cumberbatch as the devious, but sexy, Rhett Butler. The film begins ten minutes after the first one ended, when Yankee deserters from the war decide to try to take Terra by force, but Scarlet is having none of it!
