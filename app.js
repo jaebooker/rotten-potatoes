9d6c7a25a49d03019402f6df76e527b486546ae8
jaebooker <jaesonbooker@gmail.com>
2018-10-22 20:23:37 -0700

    mongoose, mongodb

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/rotten-potatoes');
+const Review = mongoose.model('Review', {
+  title: String,
+  movieTitle: String
+});
 var exphbs = require('express-handlebars');
 let reviews = [
   { title: "Amazing film. Terrontino breaves new life in the GWTW franchise, with Cumberbatch playing a devious Rhett Butler", movieTitle: "2 Gone 2 Wind" },
@@ -13,5 +19,12 @@ app.listen(3000, () => {
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.get('/', (req, res) => {
-  res.render('reviews-index', { reviews: reviews });
+    Review.find()
+    .then(reviews => {
+        res.render('reviews-index', { reviews: reviews });
+    })
+    .catch(err => {
+        console.log(err);
+    })
+  //res.render('reviews-index', { reviews: reviews });
 })
